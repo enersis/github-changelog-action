c88980c9ad4ba73da1d809ce2fa8c94cd2fbd0c5
Manni <58774455+MansurEsm@users.noreply.github.com>
2021-02-22 16:15:53 +0100

    test(): syntax error in gh-action but not locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,8 @@ GIT_JIRA_COMMITS=$(echo "$GIT_COMMITS"|tr " " "\n"|sort|uniq|tr "\n" " ")
 for i in $(echo $GIT_JIRA_COMMITS | sed "s/ / /g"); do SUMMARYLOG="$SUMMARYLOG \n $(grep $i summary.txt)\r\n"; done
 
 # Filter Jira-Storys of git commits by existing jira storys
-CHANGELOGENTRYS=$(comm -12 <(echo $GIT_JIRA_COMMITS | tr ' ' '\n' | sort) <(echo $TICKETS | tr ' ' '\n' | sort))
+# CHANGELOGENTRYS=$(comm -12 <(echo $GIT_JIRA_COMMITS | tr ' ' '\n' | sort) <(echo $TICKETS | tr ' ' '\n' | sort))
+CHANGELOGENTRYS = "test"
 CHANGELOGENTRYS=$(sed 's/^/https:\/\/enersis.atlassian.net\/browse\//; s/$//' <(echo "$CHANGELOGENTRYS") | awk '$0="<a href="$0">"$0"</a>\r\n"')
 
 # genrate changelog
